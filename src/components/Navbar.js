27d6bd9359e4cd6bf8dbe2f93c9f6d7dac3ef3af
Keertanaj <99381519+Keertanaj@users.noreply.github.com>
2023-06-01 23:17:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Logo from "../assets/pizzaLogo.png";
+import {Link} from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navbar">
         <div className="leftSide"></div>
-            <img src={} />
+            <img src={Logo} />
         <div className="rightSide"></div>
     </div>
   )
