9a9c707f14337bb3d94fca9d9e730a4195c7b7a8
Keertanaj <99381519+Keertanaj@users.noreply.github.com>
2023-06-01 21:26:36 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="leftSide"></div>
+            <img src={} />
+        <div className="rightSide"></div>
+    </div>
+  )
+}
+
+export default Navbar
