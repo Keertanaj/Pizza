2294b6d73306aacaa62c99d86793554b18463d96
Keertanaj <99381519+Keertanaj@users.noreply.github.com>
2023-06-04 14:30:27 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+            <div className="headerContainer">
+                <h1> Pedro's Pizzeria </h1>
+                <p> PIZZA TO FIT ANY TASTE</p>
+                <Link to="/menu">
+                    <button> ORDER NOW </button>
+                </Link>
+            </div>
+        </div>
   )
 }
 
